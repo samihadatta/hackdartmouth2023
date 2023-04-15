da6e1f6ab706e762ddc42e75ca59e88a019f4d0a
Elizabeth Frey <ewillowfrey@gmail.com>
2023-04-15 14:24:02 -0400

    added basic chatbot really ugly functionality

--- frontend/src/chatbot/configuration.js
+++ frontend/src/chatbot/configuration.js
@@ -1,11 +1,38 @@
-import { Configuration, OpenAIApi } from "openai";
-import dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
 
-dotenv.config();
+const key = import.meta.env.VITE_OPENAI_API_KEY;
 
 const configuration = new Configuration({
-  organization: "org-ChKEZ6qNqK0tZKK157H8txRr",
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: key,
 });
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+
+export const openai = new OpenAIApi(configuration);
+
+const makeRequest = async (req) => {
+  const completion = await openai.createCompletion({
+    model: 'gpt-3.5-turbo',
+    prompt: req,
+  });
+  return completion.data.choices[0].text;
+};
+
+export default makeRequest;
+// import axios from 'axios';
+
+// const openaiEndpoint = 'https://api.openai.com/v1/completions';
+// const headers = {
+//   'Content-Type': 'application/json',
+//   Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+// };
+
+// const makeRequest = async (req) => {
+//   const data = {
+//     model: 'gpt-3.5-turbo',
+//     messages: [{ role: 'user', content: req }],
+//   };
+//   const completion = await axios.post(openaiEndpoint, data, { headers });
+
+//   return completion.data.choices[0].text;
+// };
+
+// export default makeRequest;
