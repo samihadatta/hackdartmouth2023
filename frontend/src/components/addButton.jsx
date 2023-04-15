937ae1173ec7b4aa8b88e23bee96b59432c85152
annie-qiu <annie.r.qiu.24@dartmouth.edu>
2023-04-15 16:16:07 -0400

    modal

--- frontend/src/components/addButton.jsx
+++ frontend/src/components/addButton.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import {
-  Box, Button,
+  Box, Button, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton,
 } from '@chakra-ui/react';
+// import AddLocation from './addLocation';
 
 function AddButton() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <div>
-      <Box m={4}>
-        <Button colorScheme="blue">Add Location</Button>
+      <Box marginTop={4}>
+        <Button onClick={onOpen} colorScheme="blue">Add Location</Button>
+        <Modal isOpen={isOpen} onClose={onClose} size="xl">
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Add a Location</ModalHeader>
+            <ModalCloseButton />
+            {/* <AddLocation /> */}
+          </ModalContent>
+        </Modal>
       </Box>
     </div>
   );
