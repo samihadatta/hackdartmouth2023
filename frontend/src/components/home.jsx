a5853ba61d2a165f1e36a5df1f51b06b64b74efc
annie-qiu <annie.r.qiu.24@dartmouth.edu>
2023-04-16 11:27:30 -0400

    test

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -7,6 +7,28 @@ import Map from './map';
 
 function Home() {
   const [filter, setFilter] = useState([]);
+  fetch('https://api.mapbox.com/datasets/v1/annieqiu/clgjixyhi00w32aqjldij9nfw/features/?access_token=sk.eyJ1IjoiYW5uaWVxaXUiLCJhIjoiY2xnamphZXYwMTV5bTNtbnlhaWY4enZpZyJ9.9Eh5Jx-_vRkB8iCemvBsfw', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      type: 'Feature',
+      properties: {
+        description: 'test',
+        tag: 'safesex',
+        title: 'SWC',
+      },
+      geometry: {
+        coordinates: [
+          -74.28866117193388,
+          43.70535599162568,
+        ],
+        type: 'Point',
+      },
+    }),
+  })
+    .then((response) => console.l);
 
   const renderMap = (filter.length > 0) ? <Map filter={filter} /> : null;
 
